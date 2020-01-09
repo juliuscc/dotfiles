e87c718970e7211e4e724752af0675d6e9ffadea
juliuscc <julius.cc@hotmail.com>
2020-01-09 20:17:25 +0100

    Turn off key suggestions in vs code

--- .config-scripts/.set-defaults.sh
+++ .config-scripts/.set-defaults.sh
@@ -7,10 +7,12 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Don't pop up suggestions in hyper
+# Don't pop up suggestions in hyper or VS Code
 defaults write co.zeit.hyper ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
 
 # Quicker navigation in editors (https://apple.stackexchange.com/a/83923)
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
 
